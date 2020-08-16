c14ada1643dcff01d4689ac3eed04c6a976b2eef
Josephchalunkal <54778434+Josephchalunkal@users.noreply.github.com>
2020-08-17 02:15:35 +0530

    Updates

---
+++ precache-manifest.3325d75a882d5b244c1c0418f2249884.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb651d6b94c1050ac3fe6808cc2292d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6a9374f2f053aa89e07",
+    "url": "/static/css/main.3f923202.chunk.css"
+  },
+  {
+    "revision": "1c087e6c2b2dda6ff8dd",
+    "url": "/static/js/2.f70adc40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f70adc40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6a9374f2f053aa89e07",
+    "url": "/static/js/main.fdda8aef.chunk.js"
+  },
+  {
+    "revision": "d6f8694fa2e77b4eab35",
+    "url": "/static/js/runtime-main.2065869c.js"
+  }
+]);
